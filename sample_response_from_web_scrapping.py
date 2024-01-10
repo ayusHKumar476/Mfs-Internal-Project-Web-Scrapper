a01024d5bd628da06f7cccf8f3e7607c28a38eb9
ayusHKumar476 <mfsi.ayush@gmail.com>
2024-01-10 10:46:35 +0530

    first commit

---
+++ sample_response_from_web_scrapping.py
@@ -0,0 +1,87 @@
+[
+   {
+      "state": "Alaska",
+      "sub_locations": [
+         {
+            "city_name": "Anchorage",
+            "store_name": "1200 N. Muldoon Road",
+            "store_type": "Pizza Hut Express",
+            "store_address": "1200 N. Muldoon Road Anchorage, AK 99504 US",
+            "zip_code": "99504",
+            "store_phone_number": 0
+         },
+         {
+            "city_name": "Anchorage",
+            "store_name": "150 West 100th Ave",
+            "store_type": "Pizza Hut Express",
+            "store_address": "150 West 100th Ave Suite A Anchorage, AK 99515 US",
+            "zip_code": "99515",
+            "store_phone_number": 0
+         },
+         {
+            "city_name": "Anchorage",
+            "store_name": "2220 Abbott Rd",
+            "store_type": "Pizza Hut",
+            "store_address": "2220 Abbott Rd Anchorage, AK 99507 US",
+            "zip_code": "99507",
+            "store_phone_number": "(907) 868-0201"
+         },
+         {
+            "city_name": "Anchorage",
+            "store_name": "341 Boniface Pkwy",
+            "store_type": "Pizza Hut",
+            "store_address": "341 Boniface Pkwy Unit I Anchorage, AK 99504 US",
+            "zip_code": "99504",
+            "store_phone_number": "(907) 337-2276"
+         },
+         {
+            "city_name": "Anchorage",
+            "store_name": "3727 Spenard Highway",
+            "store_type": "Pizza Hut",
+            "store_address": "3727 Spenard Highway Anchorage, AK 99517 US",
+            "zip_code": "99517",
+            "store_phone_number": "(907) 743-9978"
+         },
+         {
+            "city_name": "Eagle River",
+            "store_name": "11740 Old Glenn Hwy",
+            "store_type": "Pizza Hut",
+            "store_address": "11740 Old Glenn Hwy Eagle River, AK 99577 US",
+            "zip_code": "99577",
+            "store_phone_number": "(907) 694-1086"
+         },
+         {
+            "city_name": "Fairbanks",
+            "store_name": "89 College Rd",
+            "store_type": "Pizza Hut",
+            "store_address": "89 College Rd Fairbanks, AK 99701 US",
+            "zip_code": "99701",
+            "store_phone_number": "(907) 451-1788"
+         },
+         {
+            "city_name": "North Pole",
+            "store_name": "301 N Santa Claus Ln",
+            "store_type": "Pizza Hut",
+            "store_address": "301 N Santa Claus Ln North Pole, AK 99705 US",
+            "zip_code": "99705",
+            "store_phone_number": "(907) 488-4239"
+         },
+         {
+            "city_name": "Soldotna",
+            "store_name": "44332 Sterling Hwy",
+            "store_type": "Pizza Hut",
+            "store_address": "44332 Sterling Hwy Soldotna, AK 99669 US",
+            "zip_code": "99669",
+            "store_phone_number": "(907) 420-0645"
+         },
+         {
+            "city_name": "Wasilla",
+            "store_name": "851 E Parks Hwy",
+            "store_type": "Pizza Hut",
+            "store_address": "851 E Parks Hwy Wasilla, AK 99654 US",
+            "zip_code": "99654",
+            "store_phone_number": "(907) 373-6600"
+         }
+      ]
+   }
+]
